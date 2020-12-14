1a4d6f7b2a06bdee3d565411c0dec590c8d48ab4
FerdiKurtSecurrency <ferdi.kurt111@gmail.com>
2020-12-14 16:55:31 +0300

    tests for ICO contract.

--- test/ico.js
+++ test/ico.js
@@ -1,8 +1,4 @@
-// todo: purchase size and price logic is wrong
-// todo: examine this and create correct logic
-
 const { expectRevert, time } = require('@openzeppelin/test-helpers');
-const { web3 } = require('@openzeppelin/test-helpers/src/setup');
 
 const ICO = artifacts.require('ICO.sol');
 const Token = artifacts.require('ERC20Token.sol');
@@ -17,195 +13,215 @@ function toWEI(arg) {
 
 contract('ICO', accounts => {
     let ico;
-    let token;
+    let tokenContract;
 
     const name = 'My Token';
     const symbol = 'TKN'; 
     const decimals = 18;
-    const initialBalance = toBN(toWEI('1000'));
+    const initialBalance = 1000
+    // const initialBalance = toBN(toWEI('1000'));
 
     const admin = accounts[0]
     const duration = 100;
-    const pricePerUnit = 2;
-    const availableTokens = toWEI('30');
-    const minPurchase = toWEI('1'); 
-    const maxPurchase = toWEI('10');
-
+    const price = 2;
+    const available = 500
+    const allowedTokens = 10
+    const percentage = 2
+    // const available = toWEI('500')
+    // const allowedTokens = toBN(toWEI('5'))
+  
     beforeEach(async () => {
         ico = await ICO.new(name, symbol, decimals, initialBalance); 
         const tokenAddress = await ico.token();
-        token = await Token.at(tokenAddress); 
+        tokenContract = await Token.at(tokenAddress); 
     });
 
     it('should create an erc20 token', async () => {
-        const _name = await token.name()
-        const _symbol = await token.symbol()
-        const _decimals = await token.decimals()
-        const _totalSupply = await token.totalSupply()
+        const _name = await tokenContract.name()
+        const _symbol = await tokenContract.symbol()
+        const _decimals = await tokenContract.decimals()
+        const _totalSupply = await tokenContract.totalSupply()
 
         assert(_name == name)
         assert(_symbol == _symbol)
         assert(_decimals == decimals)
-        assert(_totalSupply.eq(initialBalance))        
+        assert(_totalSupply == initialBalance)        
     });
 
-    //test a bit flaky because of testing of `expectedEnd`. 
-    //you can comment out the assertion on this variable to fix this
+    // test a bit flaky because of testing of `expectedEnd`. 
+    // you can comment out the assertion on this variable to fix this
     it('should start the ICO', async () => { 
-        const start = parseInt((new Date()).getTime() / 1000);
-        time.increaseTo(start);
-        await ico.start(duration, pricePerUnit, availableTokens, minPurchase, maxPurchase, { from: admin }); 
+        const start = parseInt((new Date()).getTime() / 1000)
+        time.increaseTo(start)
+        await ico.start(duration, price, available, allowedTokens, percentage, { from: admin })
     
-        const expectedEnd = start + duration;
-        const end = await ico.endOfICO();
-        const actualPrice = await ico.pricePerUnit();
-        const actualAvailableTokens = await ico.availableTokens();
-        const actualMinPurchase = await ico.minPurchase();
-        const actualMaxPurchase = await ico.maxPurchase();
+        const expectedEnd = start + duration
+        const end = await ico.endOfICO()
+        // assert(end.eq(toBN(expectedEnd)))
 
-        assert(end.eq(toBN(expectedEnd)))
-        assert(actualPrice.eq(toBN(pricePerUnit)))
-        assert(actualAvailableTokens.eq(toBN(availableTokens)))
-        assert(actualMinPurchase.eq(toBN(minPurchase)))
-        assert(actualMaxPurchase.eq(toBN(maxPurchase)))
+        const tokenPrice = await ico.pricePerToken()
+        const purchaseLimit = await ico.purchaseLimit()
+        const actualAvailableTokens = await ico.availableTokens()
+        const rate = await ico.percentage()
+       
+        assert(tokenPrice.eq(toBN(price)))
+        assert(purchaseLimit.eq(toBN(allowedTokens)))
+        assert(actualAvailableTokens.eq(toBN(available)))
+        assert(rate.toNumber() == percentage)
     });
 
-    it('should NOT start the ICO with invalid amount', async () => {
-        const invalidAmount = toBN(toWEI('10000'))
+    it('should NOT start the ICO ', async () => {
+        const invalidAmount = toWEI('1001')
+        const invalidLimit = toWEI('11')
+
         await expectRevert(
-            ico.start(duration, pricePerUnit, invalidAmount, minPurchase, maxPurchase, { from: admin }),
+            ico.start(duration, price, invalidAmount, allowedTokens, percentage, { from: admin }),
             'Invalid amount provided!'
         )
-    });
-
-    it('should NOT start the ICO with ZERO duration', async () => {
+        
         await expectRevert(
-            ico.start(0, pricePerUnit, availableTokens, minPurchase, maxPurchase, { from: admin }),
+            ico.start(0, price, available, allowedTokens, percentage, { from: admin }),
             'Invalid duration provided!'
         )
-    });
 
-    it('should NOT start the ICO with invalid price', async () => {
         await expectRevert(
-            ico.start(duration, 0, availableTokens, minPurchase, maxPurchase, { from: admin }),
-            'Invalid price proided!'
+            ico.start(duration, 0, available, allowedTokens, percentage, { from: admin }),
+            'Invalid price provided!'
         )
-    });
 
-    it('should NOT start the ICO with invalid min purchase', async () => {
         await expectRevert(
-            ico.start(duration, pricePerUnit, availableTokens, 0, maxPurchase, { from: admin }),
-            'Min must be greater than 0!'
+            ico.start(duration, price, available, invalidLimit, percentage, { from: admin }),
+            'Invalid purchase limit provided!'
         )
-    });
 
-    it('should NOT start the ICO with invalid max purchase', async () => {
-        const maxPurchase = toWEI('800')
         await expectRevert(
-            ico.start(duration, pricePerUnit, availableTokens, minPurchase, maxPurchase, { from: admin }),
-            'Invalid max purchase provided!'
+            ico.start(duration, price, available, allowedTokens, 51, { from: admin }),
+            'Invalid percentage rate is given!'
         )
     });
 
     context('Sale started', () => {
-        // beforeEach(async() => {
-        //     const start = parseInt((new Date()).getTime() / 1000);
-        //     time.increaseTo(start);
-        //     ico.start(duration, pricePerUnit, availableTokens, minPurchase, maxPurchase, { from: admin }); 
-        // });
+        beforeEach(async() => {
+            const start = parseInt((new Date()).getTime() / 1000);
+            time.increaseTo(start);
+            ico.start(duration, price, available, 250, 50, { from: admin }); 
+        });
 
-        // it('should NOT let non-investors buy', async () => {
-        //     await expectRevert(
-        //         ico.buy({ from: accounts[1], value: toWEI('10')}),
-        //         'Only investors!'
-        //     )
-        // });
+        it('should NOT let non-investors buy', async () => {
+            await expectRevert(
+                ico.buy(5, { from: accounts[1], value: 10 }),
+                'Only investors!'
+            )
+        });
 
-        // it('should NOT buy non-multiple of price', async () => {
-        //     await ico.addToWhitelist(accounts[1], { from: admin })
-        //     const value = toBN(toWEI('1'))
-        //     .add(toBN(1));
-        //     await expectRevert(
-        //         ico.buy({ from: accounts[1], value }),
-        //         'Should be multiple of price!'
-        //     )
-        // });
+        it('should NOT buy if not enough ether provided', async () => {
+            await ico.addToWhitelist(accounts[1], { from: admin })
 
-        // it('should NOT buy if not between min and max purchase', async () => {
-        //     await ico.addToWhitelist(accounts[1], { from: admin })
-        //     await expectRevert(
-        //         ico.buy({ from: accounts[1], value: toWEI('20')}),
-        //         'invalid msg.value() provided!'
-        //     )
-        // });
+            await expectRevert(
+                ico.buy(5, { from: accounts[1], value: 9 }),
+                'Not enough ether provided!'
+            )
+        });
 
-        // it('should NOT buy if not enough tokens left', async () => {
-        //     await ico.addToWhitelist(accounts[1]);
-        //     await ico.buy({from: accounts[1], value: toWEI('10')})
-        //     await ico.buy({from: accounts[1], value: toWEI('10')})
-        //     await ico.buy({from: accounts[1], value: toWEI('4')})
+        it('should NOT buy if not valid token amount provided', async () => {
+            await ico.addToWhitelist(accounts[1], { from: admin })
 
-        //     await expectRevert(
-        //         ico.buy({from: accounts[1], value: toWEI('10')}),
-        //         'Not enough tokens for sale!'
-        //     );
-        // });
+            await expectRevert(
+                ico.buy(251, { from: accounts[1], value: 502 }),
+                'Invalid token amount!'
+            )
+        });
 
-        // it.only(
-        //     'full ico process: investors buy, admin release and withdraw', 
-        //     async () => {
-        //     const [investor1, investor2] = [accounts[1], accounts[2]];
-        //     const [amount1, amount2] = [
-        //       web3.utils.toBN(web3.utils.toWei('1')),
-        //       web3.utils.toBN(web3.utils.toWei('10')),
-        //     ];
-        //     await ico.addToWhitelist(investor1);
-        //     await ico.addToWhitelist(investor2);
-        //     await ico.buy({from: investor1, value: amount1}); 
-        //     await ico.buy({from: investor2, value: amount2}); 
-      
-        //     await expectRevert(
-        //       ico.release({from: investor1}),
-        //       'Only admin!'
-        //     );
-      
-        //     await expectRevert(
-        //       ico.release(),
-        //       'ICO still active!'
-        //     );
-      
-        //     await expectRevert(
-        //       ico.withdraw(accounts[9], 10),
-        //       'ICO still active!'
-        //     );
-      
-        //     // Admin release tokens to investors
-        //     const start = parseInt((new Date()).getTime() / 1000);
-        //     time.increaseTo(start + duration + 10);
-        //     await ico.release({ from: admin });
+        it('should buy tokens', async () => {
+            await ico.addToWhitelist(accounts[1], { from: admin });
+            await ico.buy(250, {from: accounts[1], value: 500 })
+           
+            let remainingTokens = await ico.availableTokens()
+            assert.equal(remainingTokens.toNumber(), 250)
 
-        //     const balance1 = await token.balanceOf(investor1);
-        //     const balance2 = await token.balanceOf(investor2);
-        //     assert(balance1.eq(amount1.mul(web3.utils.toBN(pricePerUnit))));
-        //     assert(balance2.eq(amount2.mul(web3.utils.toBN(pricePerUnit))));
-      
-        //     await expectRevert(
-        //       ico.withdraw(accounts[9], 10, {from: investor1}),
-        //       'Only admin!'
-        //     );
-      
-        //     // Admin withdraw ether that was sent to the ico
-        //     const balanceContract = web3.utils.toBN(
-        //       await web3.eth.getBalance(token.address)
-        //     );
-        //     const balanceBefore = web3.utils.toBN(
-        //       await web3.eth.getBalance(accounts[9])
-        //     );
-        //     await ico.withdraw(accounts[9], balanceContract);
-        //     const balanceAfter = web3.utils.toBN(
-        //       await web3.eth.getBalance(accounts[9])
-        //     );
-        //     assert(balanceAfter.sub(balanceBefore).eq(balanceContract));
-        //   });
-    });
-});
+            await ico.buy(240, {from: accounts[1], value: 480 })
+            remainingTokens = await ico.availableTokens()
+
+            assert.equal(remainingTokens.toNumber(), 10)
+
+            await ico.addToWhitelist(accounts[2], { from: admin })
+
+            await expectRevert(
+                ico.buy(20, { from: accounts[2], value: 40 }),
+                'Not enough tokens for sale!'
+            )
+        });
+
+        // full process lifecycle
+        it.only('should show full lifecycle process correctly', async () => {
+            const amountOfToken = await ico.availableTokens()
+            assert.equal(amountOfToken.toNumber(), available)
+
+            await expectRevert(
+                ico.release( {from: admin }),
+                'ICO still active!'
+            )
+
+            await expectRevert(
+                ico.withdraw(accounts[9], 10, { from: admin }),
+                'ICO still active!'
+            )
+
+            const [ investor1, investor2, investor3 ] = [ accounts[1], accounts[2], accounts[3] ]
+            const [ amount1, amount2, amount3 ] = [ 250, 170, 40 ]
+
+            await ico.addToWhitelist(investor1, { from: admin })
+            await ico.addToWhitelist(investor2, { from: admin })
+            await ico.addToWhitelist(investor3, { from: admin })
+            
+            await ico.buy(amount1, { from: investor1, value: 500 })
+            await ico.buy(amount2, { from: investor2, value: 340 })
+            await ico.buy(amount3, { from: investor3, value: 80 })
+        
+            // admin release tokens
+            const start = parseInt((new Date()).getTime() / 1000);
+            time.increase(start + duration + 101)
+
+            await expectRevert(
+                ico.release({ from: investor1 }),
+                'Only admin!'
+            )
+
+            await ico.release({ from: admin });
+            const balance1 = await tokenContract.balanceOf(investor1);
+            const balance2 = await tokenContract.balanceOf(investor2);
+            const balance3 = await tokenContract.balanceOf(investor3);
+            assert(balance1.eq(toBN(250)))
+            assert(balance2.eq(toBN(170)))
+            assert(balance3.eq(toBN(40)))
+
+            inactiveTokenAmount = await ico.availableTokens()
+            assert.equal(inactiveTokenAmount.toNumber(), 40)
+            
+            // admin withdraws ether from contract
+            await expectRevert(
+                ico.withdraw(accounts[0], 10, { from: investor1 }),
+                'Only admin!'
+            )
+
+           // Admin withdraw ether that was sent to the ico
+            const balanceContract = toBN(await web3.eth.getBalance(ico.address));
+            const balanceBefore = toBN(await web3.eth.getBalance(accounts[0]));
+
+            const tx = await ico.withdraw(
+                accounts[0], 
+                balanceContract, 
+                { from: admin, gasPrice: 1 }
+            );
+
+            const gasUsed = toBN(tx.receipt.gasUsed)
+            const balanceAfter = toBN(await web3.eth.getBalance(accounts[0]));
+            assert(balanceAfter.sub(balanceBefore).add(gasUsed).eq(balanceContract));
+
+            await expectRevert(
+                ico.withdraw(accounts[9], balanceContract, { from: admin}),
+                'Invalid ether input!'
+            )
+        })
+    })
+})
