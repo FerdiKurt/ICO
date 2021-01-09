03a2f5770f2a41b39640d6f8f3c9e0bc4e366c06
FerdiKurtSecurrency <ferdi.kurt111@gmail.com>
2021-01-10 02:08:37 +0300

    refactor on optional functions.

--- contracts/IERC20Token.sol
+++ contracts/IERC20Token.sol
@@ -5,8 +5,6 @@ interface ERC20Interface {
     function transfer(address to, uint tokens) external returns (bool success);
     function transferFrom(address from, address to, uint tokens) external returns (bool success);
     function approve(address spender, uint tokens) external returns (bool success);
-    function increaseApproval(address spender, uint amount) external returns (bool success);
-    function decreaseApproval(address spender, uint amount) external returns (bool success);
     function allowance(address tokenOwner, address spender) external view returns (uint remaining);
     function balanceOf(address tokenOwner) external view returns (uint balance);
     function totalSupply() external view returns (uint);
